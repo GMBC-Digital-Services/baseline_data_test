0621a47a7761da5ecbc509199bf22a25829b15ff
Nick Lamb <nick.lamb90@googlemail.com>
2020-09-08 14:19:09 +0100

    Initial draft complete for demo. Updated notebook with case types.

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 
-#IMPORT & PREP DATA
+# IMPORT & PREP DATA
 # import the data
 data = pd.read_csv('data/Lagan_data.csv', infer_datetime_format='CREATED_DT')
 # ensure that the data/time format is in a useful format
 data['CREATED_DT'] = pd.to_datetime(data['CREATED_DT'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+data['day'] = data['CREATED_DT'].dt.dayofyear
 # make the date dd/mm/yyyy format
 data['CREATED_DT'] = data['CREATED_DT'].dt.date
 # split the classification title into a subject, reason and type using the '-'/'|' as the delimnator
@@ -52,14 +53,17 @@ overall_cases.to_csv('data/overall_cases.csv')
 class_counts = pd.pivot_table(data, index='CREATED_DT', columns='Subject', values='CASE_REFERENCE', aggfunc='count',
                               fill_value=0)
 #  Drop any columns where the sum of cases raised is less than 100
-class_counts.drop([col for col, val in class_counts.sum().iteritems() if val < 100], axis=1, inplace=True)
+class_counts.drop([col for col, val in class_counts.sum().iteritems() if val < 300], axis=1, inplace=True)
 
 # Slice to specific reportable periods
 class_covid = class_counts.loc[covid_start:covid_end]
 class_pp = class_counts.loc[pp_start:pp_end]
 
-class_covid.drop([col for col, val in class_covid.sum().iteritems() if val < 100], axis=1, inplace=True)
+class_covid.drop([col for col, val in class_covid.sum().iteritems() if val < 300], axis=1, inplace=True)
 class_covid.to_csv('data/class_covid.csv')
+class_pp.to_csv('data/class_pp.csv')
+class_covid.to_csv('notebooks/data/class_covid.csv')
+class_pp.to_csv('notebooks/data/class_pp.csv')
 
 # function to calculate rolling averages based on cases per day df
 
@@ -77,6 +81,18 @@ rolling_calculations(year_b)
 rolling_calculations(year_19)
 
 
+year_a = year_a.reset_index()
+year_a['CREATED_DT'] = pd.to_datetime(year_a['CREATED_DT'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+year_a['day'] = year_a['CREATED_DT'].dt.dayofyear
+
+year_b = year_b.reset_index()
+year_b['CREATED_DT'] = pd.to_datetime(year_b['CREATED_DT'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+year_b['day'] = year_b['CREATED_DT'].dt.dayofyear
+
 # EXPORT DATA
 year_a.to_csv('data/year_a.csv')
 year_b.to_csv('data/year_b.csv')
+year_a.to_csv('notebooks/data/year_a.csv')
+year_b.to_csv('notebooks/data/year_b.csv')
+
+#### TOP CASES
